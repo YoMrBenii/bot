0703078eeb53c898205c1b82b2c3acf3a2d53482
YoMrBenii <beniipvpzz@gmail.com>
2025-04-11 21:03:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ bot = commands.Bot(command_prefix="-", intents=intents, help_command=None, allow
 
 async def load():
     for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
+        if filename.endswith('.py') and filename != '__init__.py':
             await bot.load_extension(f'cogs.{filename[:-3]}')
 
 @bot.event
