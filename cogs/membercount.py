1005445e3ec9b822fafedd978e97cfb617231296
YoMrBenii <beniipvpzz@gmail.com>
2025-05-19 21:44:31 +0200

    Create membercount.py

---
+++ cogs/membercount.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class Membercount(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		
+	@commands.command()
+	async def mc(self, ctx):
+		mem = len(ctx.guild.members)
+		embed = discord.Embed(
+		description=f"PVP has {mem} members.", color=0xffffff, title="Membercount")
+		await ctx.send(embed=embed)
+		
+async def setup(bot):
+	await bot.add_cog(Membercount(bot))
+	
