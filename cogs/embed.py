ac9bc9a19281e9d6676513d8cae19d6dd8f7b0f8
YoMrBenii <beniipvpzz@gmail.com>
2025-05-20 21:53:41 +0200

    Update embed.py

--- cogs/embed.py
+++ cogs/embed.py
@@ -13,12 +13,6 @@ class emb(commands.Cog):
         )
         await ctx.send(embed=embed)
         
-    @commands.command()
-    async def history(self, ctx, amount: int):
-    	messages = [message async for message in ctx.channel.history(limit=amount)]
-    	message_content = "\n".join([f"{message.author}: {message.content}" for message in messages if message.content])
-    	a = discord.Embed(message_content)
-    	await ctx.send(a)
 
 async def setup(bot):
     await bot.add_cog(emb(bot))
