87e2bd8b179f46daca38904196498b539877fb9c
YoMrBenii <beniipvpzz@gmail.com>
2025-05-22 16:00:39 +0200

    Update membercount.py

--- cogs/membercount.py
+++ cogs/membercount.py
@@ -10,6 +10,7 @@ class Membercount(commands.Cog):
 		mem = len(ctx.guild.members)
 		embed = discord.Embed(
 		description=f"PVP has {mem} members.", color=0xffffff, title="Membercount")
+ embed.set_footer(text="Updates in real time.")  
 		await ctx.send(embed=embed)
 		
 async def setup(bot):
