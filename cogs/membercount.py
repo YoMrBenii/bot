ab632a63dd14c2c1e53cd3d1e18b6abc00f0115f
YoMrBenii <beniipvpzz@gmail.com>
2025-05-22 16:04:29 +0200

    Update membercount.py

--- cogs/membercount.py
+++ cogs/membercount.py
@@ -2,17 +2,17 @@ import discord
 from discord.ext import commands
 
 class Membercount(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-		
-	@commands.command()
-	async def mc(self, ctx):
-		mem = len(ctx.guild.members)
-		embed = discord.Embed(
-		description=f"PVP has {mem} members.", color=0xffffff, title="Membercount")
- embed.set_footer(text="Updates in real time.")  
-		await ctx.send(embed=embed)
-		
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mc(self, ctx):
+        mem = len(ctx.guild.members)
+        embed = discord.Embed(
+            description=f"PVP has {mem} members.", color=0xffffff, title="Membercount"
+        )
+        embed.set_footer(text="Updates in real time.")
+        await ctx.send(embed=embed)
+
 async def setup(bot):
-	await bot.add_cog(Membercount(bot))
-	
+    await bot.add_cog(Membercount(bot))
