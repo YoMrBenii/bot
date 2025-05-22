e6d4e0ef96c8057db393ad53edc9f3777f3e56a6
YoMrBenii <beniipvpzz@gmail.com>
2025-05-22 16:11:21 +0200

    Create banlist.py

---
+++ cogs/banlist.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from utils.dt import isUser
+
+class database(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    @isUser(1118218807694065684)
+    async def banlist(self, ctx):
+        MAX_CHARS = 4000
+        bans = [ban async for ban in ctx.guild.bans()]
+        banlist = "\n".join([f"{bn.user}" for bn in bans])
+        chunks = [banlist[i:i + MAX_CHARS] for i in range(0, len(banlist), MAX_CHARS)]
+        for chunk in chunks:
+        	embed = discord.Embed(description=chunk)
+        	await ctx.send(embed=embed)
+        banamount = len(bans)
+        await ctx.send(banamount)
+        
+    @banlist.error
+    async def banlist_error(self, ctx, error):
+        await ctx.send(" Something went wrong while fetching the ban list.")
+        await ctx.send(f"[banlist ERROR] {type(error).__name__}: {error}")
+
+async def setup(bot):
+    await bot.add_cog(database(bot))
