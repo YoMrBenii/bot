e41bfbd72a8f3e7a3b4ed6b174f90f78c9eb829a
YoMrBenii <beniipvpzz@gmail.com>
2025-09-05 19:29:33 +0200

    initial commit

---
+++ cogs/test.py
@@ -0,0 +1,22 @@
+import discord  
+from discord.ext import commands
+class testing(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+
+    @commands.command()
+    async def nigga(self, ctx, arg1: int, arg2: int):
+        if arg2 == None:
+            await ctx.send("provide two numbers")
+            return
+        if arg2 or arg1 != int:
+            await ctx.send("must be a number")
+            return
+        arg3 = 0
+        arg1 * arg2 = arg3
+        await ctx.send(f"{arg3} niggers")
+        
+
+async def setup(bot):
+    await bot.add_cog(testing(bot))
