9e0f2e50e2417153941529f7b0ac94c0770487a3
YoMrBenii <beniipvpzz@gmail.com>
2025-09-17 20:05:16 +0200

    F

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import discord
 
 def hasrole(member: discord.Member, roleid: list[int]) -> bool:
+    roleid = [str(r), for r in roleid]
     for role in member.roles:
         if role.id in roleid:
             return True
