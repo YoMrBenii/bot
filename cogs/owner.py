c1e5311d2ea8f46899153180f495ca8bb37421a6
YoMrBenii <beniipvpzz@gmail.com>
2025-09-28 18:41:43 +0200

    F

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from functions import owneronly
+import sys
 
 class OwnerCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -10,6 +12,18 @@ class OwnerCog(commands.Cog):
         app_info = await self.bot.application_info()
         owner = app_info.owner
         await ctx.send(f"The owner of this bot is: **{owner}** (ID: {owner.id})")
+
+    @commands.command()
+    async def shutdown(self, ctx):
+        if not owneronly(ctx.author):
+            await ctx.send("Must be beni.")
+        else:
+            await ctx.send("Bot shutting down.")
+            await self.bot.close()
+            sys.exit(0)
+
+
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(OwnerCog(bot))
