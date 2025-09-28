1cd00be6e68606045a1bee9b6e4527d015b20392
YoMrBenii <beniipvpzz@gmail.com>
2025-09-28 16:55:49 +0200

    F

--- cogs/msgs.py
+++ cogs/msgs.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from collections import defaultdict
 
 class msgcounting(commands.Cog):
-    async def __init__(self, bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
