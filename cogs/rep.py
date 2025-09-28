b2949b07fd61ec3ca471ba3e778d8a5fffb2cb6d
YoMrBenii <beniipvpzz@gmail.com>
2025-09-28 21:43:25 +0200

    F

--- cogs/rep.py
+++ cogs/rep.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from creds import *
 class reps(commands.Cog):
-    async def __init__(self, bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
