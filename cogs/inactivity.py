f7c8f1137431c3d22d79a5848ba5776715c903b9
YoMrBenii <beniipvpzz@gmail.com>
2025-09-29 23:01:56 +0200

    F

---
+++ cogs/inactivity.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+class loa(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def inactivity(self, ctx, days: int = None):
+        if days is None:
+            await ctx.send("Name how many days your going to be gone for.")
+            days = days * 86400
+            
