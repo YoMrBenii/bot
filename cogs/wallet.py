fdc5dff0cfb402eb4cec7791aca523c4f3943dcb
YoMrBenii <beniipvpzz@gmail.com>
2025-10-02 23:15:54 +0200

    F

--- cogs/wallet.py
+++ cogs/wallet.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from creds import db2, getuservar, setuservar
+from mongo import getuservar, setuservar
 from functions import owneronly
 
 
@@ -8,18 +8,6 @@ class Wallet(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
-    @commands.command()
-    async def manualwallet(self, ctx):
-        user_id = str(ctx.author.id)
-        doc_ref = db2.collection("users").document(user_id)
-        snapshot = doc_ref.get()
-        if not snapshot.exists:
-            await ctx.send("No wallet found.")
-            return
-        data = snapshot.to_dict() or {}
-        usd = data.get("usd", 0)
-        await ctx.send(f"{usd}")
         
     @commands.command()
     async def wallet(self, ctx, member: discord.Member = None):
