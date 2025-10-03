6230e7566271579463c589f3faa816940af87a4d
YoMrBenii <beniipvpzz@gmail.com>
2025-10-04 00:47:55 +0200

    GF

--- cogs/donate.py
+++ cogs/donate.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from mongo import *
 
 class donate(commands.Cog):
-    async def __init__(self, bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
