cf3b85fea0589458a453c36f12376f28677e293d
Benii <beniipvpzz@gmail.com>
2025-10-12 23:31:29 +0200

    F

--- cogs/should.py
+++ cogs/should.py
@@ -1,20 +1,26 @@
 import discord
 from discord.ext import commands
-from random import *
+from random import choice
 
-class should(commands.Cog):
+class ShouldResponder(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, message: discord.Message, ctx: commands.context):
-        lowmsg = message.lower()
-        if lowmsg != "should":
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
             return
-        texts = ["Yes king;No you nigger", "Hell yes", "Fuck no", "Yes idiot", "Yes idiot", "No idiot", "No idiot", "maybe 😏", "maybe 😏", "Yes dumbo", "Yes dumbo", "No dumbo", "No dumbo", "hmm idk ask urself r u that stupid or what be clever mf you keep skipping school go learn and get bitches", "definetly", "definetly", "Nah dont do that bro;Yeah do that man its a great idea"]
+
+        lowmsg = message.content.lower().split()
+        if not lowmsg or lowmsg[0] != "should":
+            return
+
+        texts = [
+            "Yes king", "No you nigger", "Hell yes", "Fuck no", "Yes idiot",
+            "No idiot", "maybe 😏", "Yes dumbo", "No dumbo",
+            "hmm idk ask urself r u that stupid or what be clever mf you keep skipping school go learn and get bitches",
+            "definitely", "Nah dont do that bro", "Yeah do that man its a great idea"
+        ]
+
         text = choice(texts)
-        await ctx.send(text)
-
-
-async def setup(bot):
-    await bot.add_cog(should(bot))
+        await message.channel.send(text)
